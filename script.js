812ac213da09f7cb06df88e016017bb9f13ab3f0
Jacob Feitler <jfeitler@gmail.com>
2020-12-27 19:34:33 -0500

    display is 95% correct, local storage saving correcty

--- script.js
+++ script.js
@@ -1,29 +1,45 @@
-dayjs().format()
+
 
 $(document).ready( () => {
+    dayjs().format()
     let blankDay = ['','','','','','','','','']
     let workDay = blankDay;
     let now = dayjs()
+
+    $('#currentDay').text(now.format('dddd, MMMM D, YYYY h:mm A'))
+
     for (let i = 0; i < workDay.length; i++) {
+        let relativeTime = 'future'
+        let offsetI = i+9
+        if (offsetI < now.hour()) {
+            relativeTime = 'past'
+        } else if (offsetI === now.hour()) {
+            relativeTime = 'present'
+        }
         let hourToDisplay = 
         $(`
-        <div class="row time-block">
-            <div class = "col-2 hour" id = "${i+9}">
-                ${(i+9)%12} ${i+9<12? 'am' : 'pm'}
+        <div class="row">
+            <div class = "col-2 hour" id = "hour${i}">
+                ${offsetI <= 12 ? offsetI : offsetI % 12} ${offsetI < 12 ? 'am' : 'pm'}
             </div>
-            <div class ="col-8">
-                <textarea class="form-control past" id="timeblock${i}" rows="6">
-                    ${workDay[i]}
-                </textarea>
-            </div>
-            <div class = "col-2 saveBtn">
-            <i class = "fas fa-save"></i> Save
+            
+            <textarea class="col-8 ${relativeTime} note${i}" >
+                ${workDay[i]}
+            </textarea>
+   
+            <div class = "col-2 saveBtn" save=${i}>
+                <i class = "fas fa-save"></i> Save
             </div>
         <div>
-        `
-        )
+        `);
         $('.container').append(hourToDisplay)
     }
+
+    $('.saveBtn').on('click', function() {
+        let indexOfClick = $(this).attr('save')
+        workDay[indexOfClick] = $(`.note${indexOfClick}`).val()
+        localStorage.setItem('workDay', JSON.stringify(workDay))
+    })
 })
 
 
